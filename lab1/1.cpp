1ed53e8b3ddefa4a612657416c3758628bccb5b1
nabil.alhafez <nabil.alhfz98@gmail.com>
2023-03-08 11:15:39 +0200

    Second problem

--- lab1/1.cpp
+++ lab1/1.cpp
@@ -128,25 +128,25 @@
 //}
 #include <iostream>
 using namespace std;
-
-int main() {
-    int t;
-    cin >> t;
-
-    while (t--) {
-        long long int xP, yP, xQ, yQ, xR, yR;
-        cin >> xP >> yP >> xQ >> yQ >> xR >> yR;
-
-        long long int produs_vectorial = (long long)(xQ - xP) * (yR - yP) - (long long)(xR - xP) * (yQ - yP);
-
-        if (produs_vectorial < 0) {
-            cout << "RIGHT\n";
-        } else if (produs_vectorial > 0) {
-            cout << "LEFT\n";
-        } else {
-            cout << "TOUCH\n";
-        }
-    }
-
-    return 0;
-}
+//
+//int main() {
+//    int t;
+//    cin >> t;
+//
+//    while (t--) {
+//        long long int xP, yP, xQ, yQ, xR, yR;
+//        cin >> xP >> yP >> xQ >> yQ >> xR >> yR;
+//
+//        long long int produs_vectorial = (long long)(xQ - xP) * (yR - yP) - (long long)(xR - xP) * (yQ - yP);
+//
+//        if (produs_vectorial < 0) {
+//            cout << "RIGHT\n";
+//        } else if (produs_vectorial > 0) {
+//            cout << "LEFT\n";
+//        } else {
+//            cout << "TOUCH\n";
+//        }
+//    }
+//
+//    return 0;
+//}
--- lab1/2.cpp
+++ lab1/2.cpp
@@ -1,33 +1,70 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//
-//#define ll long long
-//
-//using namespace std;
-//
-//bool compareInterval(pair<int, int> doc1, pair<int, int> doc2) {
-//    return (doc1.second - doc1.first + 1) > (doc2.second - doc2.first - 1);
-//}
-//
-//int main() {
-//
-//    int t;
-//    int a, b;
-//    cin >> a >> b;
-//    vector<pair<int, int>> doctors;
-//    vector<pair<int, int>> res;
-//    cin >> t;
-//    int N = t;
-//    while (t--) {
-//        ll x, y;
-//        cin >> x >> y;
-//        doctors.emplace_back(x, y);
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+
+struct Point {
+    long long int x, y;
+};
+
+int orientation(Point p1, Point p2, Point p3) {
+    long long int val = (long long int) (p2.y - p1.y) * (p3.x - p2.x) - (long long int) (p2.x - p1.x) * (p3.y - p2.y);
+    if (val == 0) return 0;  // colinear
+    return (val > 0) ? 1 : 2; // clock or counterclock wise
+}
+
+
+void solve() {
+    int n;
+//    ifstream cin("/Users/nabil.alhafez/CLionProjects/AlgoAvan/lab1/TestePubliceLab1/roby/roby12.in");
+//    ifstream fout("/Users/nabil.alhafez/CLionProjects/AlgoAvan/lab1/TestePubliceLab1/roby/roby12.out");
+
+    cin >> n;
+
+    vector<Point> points(n);
+    for (int i = 0; i < n; i++) {
+        cin >> points[i].x >> points[i].y;
+    }
+
+    int left_turns = 0, right_turns = 0, straight = 0;
+    for (int i = 0; i < n - 2; i++) {
+        int o = orientation(points[i], points[i + 1], points[i + 2]);
+        if (o == 0) {
+            straight++;
+        } else if (o == 1) {
+            right_turns++;
+        } else {
+            left_turns++;
+        }
+    }
+    // check orientation of last two points to determine final turn
+    int o = orientation(points[n - 2], points[n - 1], points[0]);
+    if (o == 0) {
+        straight++;
+    } else if (o == 1) {
+        right_turns++;
+    } else {
+        left_turns++;
+    }
+
+//    int expectedLeftTurns, expectedRightTurns, expectedSameDir;
+//    fout >> expectedLeftTurns >> expectedRightTurns >> expectedSameDir;
+//    fout.close();
+//    // Verify if calculated answer matches expected answer
+//    if (left_turns == expectedLeftTurns && right_turns == expectedRightTurns && straight == expectedSameDir) {
+//        cout << "Test passed" << endl;
+//    } else {
+//        cout << "Test failed" << endl;
+//        cout << "Expected answer: " << expectedLeftTurns << " " << expectedRightTurns << " " << expectedSameDir << endl;
+//        cout << "Calculated answer: " << left_turns << " " << right_turns << " " << straight << endl;
 //    }
-//    sort(doctors.begin(), doctors.end(), compareInterval);
-//    for (auto ans: doctors) {
-//        doctors;
-//        cout << ans.first << ' ' << ans.second << endl; }
-//
-//    return 0;
-//}
+    cout << left_turns << " " << right_turns << " " << straight << endl;
+}
+
+int main() {
+    solve();
+    return 0;
+}
+
